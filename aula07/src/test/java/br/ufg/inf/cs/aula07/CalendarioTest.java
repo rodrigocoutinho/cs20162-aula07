d8bd756f25dc526f2e03e18f374d3f10fdca27c2
Rodrigo F. Coutinho <cvc.francisco@gmail.com>
2016-10-22 21:18:53 -0200

    UP testes

--- aula07/src/test/java/br/ufg/inf/cs/aula07/CalendarioTest.java
+++ aula07/src/test/java/br/ufg/inf/cs/aula07/CalendarioTest.java
@@ -5,6 +5,7 @@
  */
 package br.ufg.inf.cs.aula07;
 
+import java.time.LocalDate;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -13,7 +14,7 @@ import static org.junit.Assert.*;
  * @author Shakaw
  */
 public class CalendarioTest {
-    
+
     int num = 20161019;
 
     public CalendarioTest() {
@@ -26,16 +27,16 @@ public class CalendarioTest {
     @Test
     public void testAno() {
         Calendario.ano(num);
-        assertEquals(2016,Calendario.ano(num));
+        assertEquals(2016, Calendario.ano(num));
     }
-    
+
     /**
      * Test of ano method, of class Calendario.
      */
     @Test
     public void testAno2() {
         Calendario.ano(num);
-        
+
     }
 
     /**
@@ -45,7 +46,7 @@ public class CalendarioTest {
     public void testMes() {
         Calendario.mes(num);
     }
-    
+
     /**
      * Test of mes method, of class Calendario.
      */
@@ -61,12 +62,28 @@ public class CalendarioTest {
     public void testDia() {
         Calendario.dia(num);
     }
-    
+
     /**
      * Test of dia method, of class Calendario.
      */
     @Test
     public void testDia2() {
         assertEquals(19, Calendario.dia(num));
+    }
+
+    @Test
+    public void umDiaArbitrarioObtidoDeCalendario() {
+        assertEquals(2, Calendario.diaDaSemana(2016, 8, 10));
+    }
+
+    @Test
+    public void umAnoBissexto() {
+        assertEquals(0, Calendario.diaDaSemana(2016, 2, 29));
+    }
+
+    @Test
+    public void umDiaDistanteParaOsHumanos() {
+        int diaDaSemana = LocalDate.of(5000, 1, 1).getDayOfWeek().ordinal();
+        assertEquals(diaDaSemana, Calendario.diaDaSemana(5000, 1, 1));
     }
 }
